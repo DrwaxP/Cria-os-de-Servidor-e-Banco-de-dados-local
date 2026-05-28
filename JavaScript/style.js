1c12ebee7880dacee4d18b2bb7e84f48d7472e15
_Drwax_P <161478981+DrwaxP@users.noreply.github.com>
2026-05-28 16:07:31 -0300

    Delete JavaScript/style.js

--- JavaScript/style.js
+++
@@ -1,9 +0,0 @@
-function enviarDados() {
-    const nome = document.querySelector('.nome');
-    const idade = document.querySelector('.idade');
-    const email = document.querySelector('.email');
-    const senha = document.querySelector('.senha');
-
-    const resultadoDiv = document.querySelector('.resultado');
-    resultadoDiv.innerHTML = `<p>Nome: ${nome}</p><p>Idade: ${idade}</p><p>Email: ${email}</p><p>Senha: ${senha}</p>`;
-}
